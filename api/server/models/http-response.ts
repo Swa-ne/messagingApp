8268fd15032b16dd967ef4d02fcc0abfef3795dc
swa-ne <spaulbautista@gmail.com>
2024-05-07 22:40:38 +0800

    Remove HttpResponse class on entry.ts files of controller and models.

--- api/server/models/http-response.ts
+++
@@ -1,9 +0,0 @@
-export class HttpResponse {
-    code: number;
-    message: any;
-    constructor(message: any, errorCode: number) {
-        this.message = message;
-        this.code = errorCode;
-    }
-}
-// module.exports = HttpResponse
