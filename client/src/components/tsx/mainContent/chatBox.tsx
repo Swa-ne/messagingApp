27183551eb000aefa529b2cbeaaab657d093fc9e
swa-ne <spaulbautista@gmail.com>
2024-05-09 18:30:52 +0800

    Created the part where users see their conversation

--- client/src/components/tsx/mainContent/chatBox.tsx
+++ client/src/components/tsx/mainContent/chatBox.tsx
@@ -1,10 +1,12 @@
 import ChatBoxHeader from "./chatBox/header";
+import MainChat from "./chatBox/mainChat";
 
 export default function ChatBox() {
     return (
         <div className='Chat-box w-[64%] flex justify-between items-center p-2 flex-col '>
             <div className='w-full h-full bg-gray-900'>
                 <ChatBoxHeader />
+                <MainChat />
             </div>
         </div>
     );
---
+++ client/src/components/tsx/mainContent/chatBox/mainChat.tsx
@@ -0,0 +1,12 @@
+import ReceiverBox from "./mainChat/receiver";
+import SenderBox from "./mainChat/sender";
+
+export default function MainChat() {
+    return (
+        <div className='w-auto h-[85%] m-1 cursor-default overflow-y-scroll'>
+            <div className="w-full flex flex-col justify-end">
+
+            </div>
+        </div>
+    );
+}
