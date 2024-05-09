ce2d5957a63ec50f83b628b5351288944c3b3067
swa-ne <spaulbautista@gmail.com>
2024-05-09 18:51:21 +0800

    Small update on sender.tsx, and made the main chat to start at the bottom of the conversation.

--- client/src/components/tsx/mainContent/chatBox/mainChat.tsx
+++ client/src/components/tsx/mainContent/chatBox/mainChat.tsx
@@ -1,11 +1,32 @@
+import { useEffect, useRef } from "react";
 import ReceiverBox from "./mainChat/receiver";
 import SenderBox from "./mainChat/sender";
 
 export default function MainChat() {
-    return (
-        <div className='w-auto h-[85%] m-1 cursor-default overflow-y-scroll'>
-            <div className="w-full flex flex-col justify-end">
+    const containerRef = useRef<HTMLDivElement>(null);
 
+    // Function to scroll to the bottom
+    const scrollToBottom = () => {
+        if (containerRef.current) {
+            containerRef.current.scrollTop = containerRef.current.scrollHeight;
+        }
+    };
+    useEffect(() => {
+        scrollToBottom();
+    }, []);
+    return (
+        <div className='w-auto h-[85%] m-1 cursor-default overflow-y-scroll' ref={containerRef}>
+            <div className="w-full flex flex-col justify-end">
+                <SenderBox message="fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1" timestamp="a" />
+                <SenderBox message="fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1" timestamp="a" />
+                <SenderBox message="fasdfdad1" timestamp="a" />
+                <ReceiverBox isLatest={true} profile='https://i.pinimg.com/originals/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg' message="fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1" timestamp="a" />
+                <SenderBox message="fasdfdad1" timestamp="a" />
+                <ReceiverBox profile='https://i.pinimg.com/originals/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg' message="fasdfasf" timestamp="a" />
+                <ReceiverBox profile='https://i.pinimg.com/originals/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg' message="fasdfasf" timestamp="a" />
+                <ReceiverBox profile='https://i.pinimg.com/originals/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg' message="fasdfasf" timestamp="a" />
+                <ReceiverBox profile='https://i.pinimg.com/originals/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg' message="fasdfasf" timestamp="a" />
+                <ReceiverBox isLatest={true} profile='https://i.pinimg.com/originals/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg' message="fasdfasf" timestamp="a" />
             </div>
         </div>
     );
--- client/src/components/tsx/mainContent/chatBox/mainChat/sender.tsx
+++ client/src/components/tsx/mainContent/chatBox/mainChat/sender.tsx
@@ -4,7 +4,7 @@ interface SenderChatProps {
 }
 export default function SenderBox({ message, timestamp }: SenderChatProps) {
     return (
-        <div className='w-[80%] rounded-xl rounded-br-sm p-2 m-[1px] self-end bg-blue-400'>
+        <div className='w-[80%] rounded-xl rounded-br-sm p-2 m-[1px] mr-2 self-end bg-blue-400'>
             <p className="whitespace-pre-wrap break-all">
                 {message}
             </p>
