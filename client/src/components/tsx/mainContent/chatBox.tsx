5576f9b7333e0d280e357cdbc0c5d6c45c1a9fbb
swa-ne <spaulbautista@gmail.com>
2024-05-10 15:44:20 +0800

    Changed chatbox width to make space for the chat details

--- client/src/components/tsx/mainContent/chatBox.tsx
+++ client/src/components/tsx/mainContent/chatBox.tsx
@@ -4,7 +4,7 @@ import MainChat from "./chatBox/mainChat";
 
 export default function ChatBox() {
     return (
-        <div className='Chat-box w-[64%] flex justify-between items-center p-2 flex-col '>
+        <div className='chat-box min-w-[60%] h-98p flex justify-between items-center p-2 flex-col'>
             <div className='w-full h-full bg-gray-900 flex flex-col'>
                 <ChatBoxHeader />
                 <MainChat />
