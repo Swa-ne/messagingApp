1ee67253248c6fca742c650b67273aa59442339a
swa-ne <spaulbautista@gmail.com>
2024-05-11 17:04:15 +0800

    Installed Mongoose and connected my API to the MongoDB Atlas database

--- api/server/index.ts
+++ api/server/index.ts
@@ -1,11 +1,22 @@
-import express, {Express, Request, Response} from "express"
+import express, { Express, Request, Response } from "express"
 import bodyParser from "body-parser"
 
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 dotenv.config()
 
 const entryRoutes = require("./routes/entry")
 
+const MONGODB_CONNECTION: any = process.env.MONGODB_CONNECTION;
+
+mongoose
+  .connect(MONGODB_CONNECTION)
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('Internal Server Error');
+  });
 
 const app = express();
 const port = 3000;
