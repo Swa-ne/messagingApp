4f84ab187eb7c51388272ead92fd334d0d448a4c
swa-ne <spaulbautista@gmail.com>
2024-05-16 18:28:17 +0800

    Connected client socket-io to the backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,26 @@ import './App.scss';
 import Login from './pages/login';
 import Loading from './pages/loading';
 import MainContent from './pages/mainContent';
-import { authenticateToken } from './services/entry';
+import { authenticateToken, cookies } from './services/entry';
+import { io, Socket } from 'socket.io-client';
 
 function App() {
     const [isLoading, setIsLoading] = useState(true);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [socket, setSocket] = useState<Socket | null>(null);
 
     useEffect(() => {
         authenticateToken()
             .then((isValid) => {
                 setIsLoggedIn(isValid);
+                if (isValid) {
+                    const newSocket = io("http://localhost:3000", {
+                        query: {
+                            userId: cookies.get("userID")
+                        },
+                    });
+                    setSocket(newSocket)
+                }
             })
             .catch((e) => console.log(e))
             .finally(() => {
@@ -20,7 +30,7 @@ function App() {
             });
     }, []);
 
-    return <>{isLoading ? <Loading /> : <>{isLoggedIn ? <MainContent /> : <Login />}</>}</>;
+    return <>{isLoading ? <Loading /> : <>{isLoggedIn ? <MainContent socket={socket} /> : <Login />}</>}</>;
 }
 
 export default App;
