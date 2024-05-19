69a50726ceccc0bc80c784616ec605775dd38532
swa-ne <spaulbautista@gmail.com>
2024-05-19 21:28:50 +0800

    Removed mock data from the main chat to test the implementation of the real data about the messages in a conversation

--- client/src/components/tsx/mainContent/chatBox/mainChat.tsx
+++ client/src/components/tsx/mainContent/chatBox/mainChat.tsx
@@ -1,10 +1,15 @@
 import { useEffect, useRef } from "react";
 import ReceiverBox from "./mainChat/receiver";
 import SenderBox from "./mainChat/sender";
+import { Chat } from "../../../../types/chat";
+import { cookies } from "../../../../services/entry";
 
-export default function MainChat() {
+interface MainChatProps {
+    messages: Chat[]
+}
+export default function MainChat({ messages }: MainChatProps) {
     const containerRef = useRef<HTMLDivElement>(null);
-
+    const userId = cookies.get("userId")
     // Function to scroll to the bottom
     const scrollToBottom = () => {
         if (containerRef.current) {
@@ -17,16 +22,12 @@ export default function MainChat() {
     return (
         <div className='w-auto max-h-[85%] m-1 cursor-default overflow-y-scroll' ref={containerRef}>
             <div className="w-full flex flex-col justify-end">
-                <SenderBox message="fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1" timestamp="a" />
-                <SenderBox message="fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1" timestamp="a" />
-                <SenderBox message="fasdfdad1" timestamp="a" />
-                <ReceiverBox isLatest={true} profile='https://i.pinimg.com/originals/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg' message="fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1fasdfdad1" timestamp="a" />
-                <SenderBox message="fasdfdad1" timestamp="a" />
-                <ReceiverBox profile='https://i.pinimg.com/originals/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg' message="fasdfasf" timestamp="a" />
-                <ReceiverBox profile='https://i.pinimg.com/originals/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg' message="fasdfasf" timestamp="a" />
-                <ReceiverBox profile='https://i.pinimg.com/originals/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg' message="fasdfasf" timestamp="a" />
-                <ReceiverBox profile='https://i.pinimg.com/originals/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg' message="fasdfasf" timestamp="a" />
-                <ReceiverBox isLatest={true} profile='https://i.pinimg.com/originals/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg' message="fasdfasf" timestamp="a" />
+                {messages.map((message: Chat) => {
+                    if (message.sender === userId) {
+                        return <SenderBox {...message} />
+                    }
+                    return <ReceiverBox isLatest={true} profile='https://i.pinimg.com/originals/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg' {...message} />
+                })}
             </div>
         </div>
     );
