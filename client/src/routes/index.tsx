e09558e5dd7bcf85008fa45ebe015d222c8a635b
swa-ne <spaulbautista@gmail.com>
2024-05-21 21:37:28 +0800

    Updated routes

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import ChatContainer from '../components/tsx/mainContent/threadList/chatContainer';
 import ActiveContainer from '../components/tsx/mainContent/threadList/activeContainer';
+import DefaultProps from '../types/defaultProps';
 
 
-export default function ThreadListRoutes() {
+export default function ThreadListRoutes({ socket }: DefaultProps) {
     return (
         <Routes>
-            <Route path='/*' element={<ChatContainer />} />
-            <Route path='/active*' element={<ActiveContainer />} />
+            <Route path='/' element={<ChatContainer socket={socket} />} />
+            <Route path='/:id' element={<ChatContainer socket={socket} />} />
+            <Route path='/active/' element={<ActiveContainer socket={socket} />} />
+            <Route path='/active/:id' element={<ActiveContainer socket={socket} />} />
         </Routes>
     );
 }
