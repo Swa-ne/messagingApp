ee5582a13af00d41b64ab98d6083ce8e0d699e41
swa-ne <spaulbautista@gmail.com>
2024-05-23 23:38:20 +0800

    Fixed the layout of the sender and receiver chat bubbles

--- client/src/components/tsx/mainContent/chatBox/mainChat/receiver.tsx
+++ client/src/components/tsx/mainContent/chatBox/mainChat/receiver.tsx
@@ -1,20 +1,23 @@
+import { timestampToDate } from "../../../../../utils/convertTime";
+
 interface ReceiverChatProps {
     profile: string,
     message: string,
-    timestamp: string,
-    isLatest?: boolean
+    createdAt: string,
+    isLatest?: boolean,
+    fullName: string
 }
-export default function ReceiverBox({ profile, message, timestamp, isLatest = false }: ReceiverChatProps) {
+export default function ReceiverBox({ profile, message, createdAt, isLatest = false, fullName }: ReceiverChatProps) {
     return (
-        <div className="w-[80%] flex">
+        <div className="max-w-[80%] flex">
             <div className="w-[30px] h-[30px] mr-2 relative self-end">
                 {isLatest &&
-                    <div className="w-full h-full object-cover rounded-full overflow-hidden">
+                    <div className="w-full h-full object-cover rounded-full overflow-hidden" title={fullName}>
                         <img src={profile} className="w-full h-full object-cover" />
                     </div>
                 }
             </div>
-            <div className='w-full rounded-xl rounded-bl-sm p-2 m-[1px] self-start bg-gray-500'>
+            <div className='max-w-[80%] rounded-xl rounded-bl-sm p-2 m-[1px] self-start bg-gray-500' title={timestampToDate(createdAt)}>
                 <p className="whitespace-pre-wrap break-all">
                     {message}
                 </p>
--- client/src/components/tsx/mainContent/chatBox/mainChat/sender.tsx
+++ client/src/components/tsx/mainContent/chatBox/mainChat/sender.tsx
@@ -1,10 +1,12 @@
+import { timestampToDate } from "../../../../../utils/convertTime";
+
 interface SenderChatProps {
     message: string,
-    timestamp: string
+    createdAt: string
 }
-export default function SenderBox({ message, timestamp }: SenderChatProps) {
+export default function SenderBox({ message, createdAt }: SenderChatProps) {
     return (
-        <div className='w-[80%] rounded-xl rounded-br-sm p-2 m-[1px] mr-2 self-end bg-blue-400'>
+        <div className='max-w-[80%] rounded-xl rounded-br-sm p-2 m-[1px] mr-2 self-end bg-blue-400' title={timestampToDate(createdAt)}>
             <p className="whitespace-pre-wrap break-all">
                 {message}
             </p>
