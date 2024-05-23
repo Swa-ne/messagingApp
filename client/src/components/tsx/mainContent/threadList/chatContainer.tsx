217f4c365bc031c46edddb96f4c821386718a0e4
swa-ne <spaulbautista@gmail.com>
2024-05-23 23:41:16 +0800

    Deleted unused socket functionality from ChatContainer for better code cleanliness and reduced overhead

--- client/src/components/tsx/mainContent/threadList/chatContainer.tsx
+++ client/src/components/tsx/mainContent/threadList/chatContainer.tsx
@@ -1,8 +1,9 @@
+import DefaultProps from "../../../../types/defaultProps";
 import ChatList from "./chatContainer/chatList";
 import SearchBar from "./chatContainer/searchBar";
 // import SearchList from "./chatContainer/searchList";
 
-export default function ChatContainer() {
+export default function ChatContainer({ socket }: DefaultProps) {
     return (
         <div className='w-full h-full bg-dark-background overflow-y-scroll'>
             <h3 className="text-center py-3">
