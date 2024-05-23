33b43f41946dc5dbe0e7a54d88d44fe97b4aaa3d
swa-ne <spaulbautista@gmail.com>
2024-05-23 23:25:04 +0800

    Added a backend socket where users can send and receive messages

--- api/server/sockets/socketServer.ts
+++ api/server/sockets/socketServer.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import { Server } from "socket.io";
 import http from "http";
-import { ChangeUsersStatusToActive, ChangeUsersStatusToInactive, CheckUserStatusIfActive } from "../services/active.users";
+import { ChangeUsersStatusToActive, ChangeUsersStatusToInactive, CheckUserStatusIfActive, GetSocketId } from "../services/active.users";
 import { getAllActiveUsers } from "../services/user";
 
 const app = express();
@@ -15,24 +15,27 @@ const io = new Server(server, {
 });
 
 io.on("connection", async (socket) => {
-    console.log("a user connected", socket.id);
 
     const userId = socket.handshake.query.userId;
     try {
         const isActive = await CheckUserStatusIfActive(userId);
-        console.log("User active status:", isActive);
 
         if (!isActive) {
             await ChangeUsersStatusToActive(userId, socket.id);
-            console.log(`User status changed to active for userId: ${userId}, socketId: ${socket.id}`);
         }
     } catch (error) {
         console.error("Error during user status check or update:", error);
     }
     io.emit("getOnlineUsers", await getAllActiveUsers());
 
+    socket.on("send-msg", async (data) => {
+        if (await CheckUserStatusIfActive(data.userId)) {
+            const socketId = await GetSocketId(data.userId)
+            if (socketId) socket.to(socketId).emit("msg-recieve", data.msg);
+        }
+    });
+
     socket.on("disconnect", async () => {
-        console.log("user disconnected", socket.id);
         await ChangeUsersStatusToInactive(userId)
         io.emit("getOnlineUsers", await getAllActiveUsers());
     });
