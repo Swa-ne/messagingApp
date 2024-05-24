3cbe02920d1287f583f2837d302385cf6282df23
swa-ne <spaulbautista@gmail.com>
2024-05-25 01:02:02 +0800

    Removed mock data and PersonSearch component imports from SearchList

--- client/src/components/tsx/mainContent/threadList/chatContainer/searchList.tsx
+++ client/src/components/tsx/mainContent/threadList/chatContainer/searchList.tsx
@@ -1,48 +1,11 @@
-import { PersonChatProps } from "../../../../../types/chat";
-import PersonSearch from "./personSearch";
-
-const mockData: PersonChatProps[] = [
-    {
-        profile: 'https://i.pinimg.com/originals/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg',
-        name: 'John FFD df sdFD',
-        chat: {
-            message: 'Hello there!',
-            sender: 'John',
-            receiver: 'Jane',
-            isRead: true,
-            timestamp: '2024-05-08T12:00:00'
-        }
-    },
-    {
-        profile: 'https://i.pinimg.com/originals/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg',
-        name: 'Jane',
-        chat: {
-            message: 'Hi John!',
-            sender: 'Jane',
-            receiver: 'John',
-            isRead: true,
-            timestamp: '2024-05-08T12:01:00'
-        }
-    },
-    {
-        profile: 'https://i.pinimg.com/originals/58/51/2e/58512eb4e598b5ea4e2414e3c115bef9.jpg',
-        name: 'Alice',
-        chat: {
-            message: 'Hey folks!',
-            sender: 'Alice',
-            receiver: 'Bob',
-            isRead: false,
-            timestamp: '2024-05-08T12:02:00'
-        }
-    }
-];
-
+// import { PersonChatProps } from "../../../../../types/chat";
+// import PersonSearch from "./personSearch";
 export default function SearchList() {
     return (
         <div className='w-full bg-dark-background flex flex-col items-center py-3'>
-            {mockData.map((data, idx) =>
+            {/* {mockData.map((data, idx) =>
                 <PersonSearch key={idx} {...data} />
-            )}
+            )} */}
         </div>
     );
 }
